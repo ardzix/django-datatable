7aff132ea67a6ea217de63b84c6a69e440f12b38
ardzix <ardzyix@gmail.com>
2018-03-10 15:46:42 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.2.1',
 
-    description='Ardzix's datatable django serverside',
+    description='Ardzix datatable django serverside',
     long_description=long_description,
 
     # The project's main homepage.
